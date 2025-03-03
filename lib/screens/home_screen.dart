b80da60ce83c0db2d9001a8b82f27e54970aa6e0
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-03 00:14:43 -0300

    Refactor HomeScreen: replace quantity management with a product list for better scalability and maintainability

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,24 +9,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _quantity = 1;
-  final double _pricePerUnit = 6.79;
+  List<Map<String, dynamic>> products = [
+    {
+      "imageUrl":
+          'https://merconnect-production.s3.amazonaws.com/uploads/products/221_200215_5cd93d7a-ae5f-4bb1-81a2-51131d1b6e64.png',
+      "productName": 'Coca Cola 1l',
+      "productDetails": 'Sabor Original',
+      "pricePerUnit": 6.79,
+      "quantity": 1,
+    },
+    {
+      "imageUrl":
+          'https://merconnect-production.s3.amazonaws.com/uploads/products/7894900031515-min.png',
+      "productName": 'Fanta Laranja 2l',
+      "productDetails": 'Sabor Original',
+      "pricePerUnit": 8.79,
+      "quantity": 1,
+    },
+  ];
 
-  void _incrementQuantity() {
+  void _incrementQuantity(int index) {
     setState(() {
-      _quantity++;
+      products[index]["quantity"]++;
     });
   }
 
-  void _decrementQuantity() {
-    if (_quantity > 1) {
+  void _decrementQuantity(int index) {
+    if (products[index]["quantity"] > 1) {
       setState(() {
-        _quantity--;
+        products[index]["quantity"]--;
       });
     }
   }
-
-  double get totalPrice => _quantity * _pricePerUnit;
 
   void _handleAddressTap() {
     debugPrint('Endereço tocado!');
@@ -53,16 +67,27 @@ class _HomeScreenState extends State<HomeScreen> {
                     const CarouselWidget(indicatorColor: Colors.red),
                     const SizedBox(height: 10),
                     const CategoriesWidget(),
-                    ProductCard(
-                      imageUrl:
-                          'https://merconnect-production.s3.amazonaws.com/uploads/products/221_200215_5cd93d7a-ae5f-4bb1-81a2-51131d1b6e64.png',
-                      productName: 'Coca Cola 1l',
-                      productDetails: 'Sabor Original',
-                      pricePerUnit: _pricePerUnit,
-                      quantity: _quantity,
-                      onIncrement: _incrementQuantity,
-                      onDecrement: _decrementQuantity,
-                      onAdd: () {},
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        final product = products[index];
+                        return ProductCard(
+                          imageUrl: product["imageUrl"],
+                          productName: product["productName"],
+                          productDetails: product["productDetails"],
+                          pricePerUnit: product["pricePerUnit"],
+                          quantity: product["quantity"],
+                          onIncrement: () => _incrementQuantity(index),
+                          onDecrement: () => _decrementQuantity(index),
+                          onAdd: () {
+                            debugPrint(
+                              '${product["productName"]} adicionado ao carrinho!',
+                            );
+                          },
+                        );
+                      },
                     ),
                     const SizedBox(height: 100),
                   ],
