bbd853e9e1b6396478a8a0b3885326bcb0dc12d5
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-03-03 10:35:35 -0300

    Refactor SearchScreen: convert to StatefulWidget and add animated gradient header

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:idrink/widgets.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +34,33 @@ class SearchScreen extends StatelessWidget {
       child: Scaffold(
         appBar: CustomAppBar(showBackButton: false),
         body: Column(
-          children: const [
-            SizedBox(height: 20),
-            // CarouselWidget(indicatorColor: Colors.red),
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.red, Colors.amber],
+                      begin: Alignment(-1 + _controller.value, 0),
+                      end: Alignment(_controller.value, 0),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Para você",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
